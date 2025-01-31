c4c52268a5a91f5bd61dd3cd8af18aceef135e9b
Arnel Imperial <arnel@Arnels-MacBook-Pro.local>
2025-01-31 10:12:44 +0800

    :tada: (project) first commit

---
+++ src/main/java/com/imperionite/inventorysystem/InventorySystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.imperionite.inventorysystem;
+
+/**
+ *
+ * @author arnel
+ */
+public class InventorySystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
